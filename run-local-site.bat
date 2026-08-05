d6573910ce14266a3a3736f27ade26411c494f14
serand2048 <serand228@gmail.com>
2026-08-05 18:54:56 +0300

    fix

--- run-local-site.bat
+++ run-local-site.bat
@@ -5,7 +5,7 @@ echo Starting local Jekyll server...
 echo Close this window when done.
 echo.
 
-start "" cmd /c "timeout /t 8 >nul && start http://127.0.0.1:4000/"
+start "" cmd /c "timeout /t 15 >nul && start http://127.0.0.1:4000/"
 
 bundle exec jekyll serve
 
